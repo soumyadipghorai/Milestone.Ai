a567c5c3853359fc796b28aea5254ad0ab0ab40d
soumyadipghorai <ghorai.soumyadip33@gmail.com>
2025-01-03 23:38:06 +0530

    production url added

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000/v1", 
+  // baseURL: "http://localhost:8000/v1", 
+  baseURL: "https://milestone-ai.onrender.com/docs", 
   headers: {
     "Content-Type": "application/json",
   },
